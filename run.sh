681772321b062d900fc23e93db5de3568bff6be5
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-11-16 13:41:34 -0800

    Script updates

--- run.sh
+++ run.sh
@@ -22,6 +22,6 @@ else
 	fi
 fi
 
-com="sudo docker run --rm -d -v $map_dir:/data -p $port:80 klokantech/tileserver-gl $map_file > docker_id"
-sudo docker run --rm -d -v $map_dir:/data -p $port:80 klokantech/tileserver-gl $map_file > docker_id
+com="sudo docker run --rm -d -v $map_dir:/data -p $port:80 klokantech/tileserver-gl $map_file"
+$com > docker_id
 echo 'Server launched. Use ./kill.sh to finish server.'
